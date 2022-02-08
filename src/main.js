ea916f82754b7ced45b6dcfb869babcf5ed4b334
Lynn262 <xiely2000@sina.cn>
2022-02-08 12:08:13 +0800

    yaoyao learn

--- src/main.js
+++ src/main.js
@@ -3,4 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// const app = createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(router)
+app.use(store)
+app.mount("#app")
