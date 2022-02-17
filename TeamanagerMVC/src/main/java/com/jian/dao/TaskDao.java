08cdb816f857238bc0da74ff6771832e62d56e63
qixiaojian310 <1821059387@qq.com>
2022-02-17 17:52:13 +0800

    yaoyaoupdate2

--- TeamanagerMVC/src/main/java/com/jian/dao/TaskDao.java
+++ TeamanagerMVC/src/main/java/com/jian/dao/TaskDao.java
@@ -8,11 +8,14 @@ import java.sql.Date;
 import java.util.List;
 
 public interface TaskDao {
-    public List<Task> getTaskList();
+    //这个地方只能获取除了组员之外的task信息
+    public Task getTask(String taskId);
+    //获取该task的组员信息
     public  List<Student>  getStudentList(String taskId);
-    public String getContext(String taskId);
-    public String getTeamId(String taskId);
-    public Date getDeadline(String taskId);
+    //增加一个空的任务
+    public boolean addTask(Task task);
+    //给该任务添加学生
+    public boolean addTaskStudent(String taskId,String studentId);
 
 
 }
