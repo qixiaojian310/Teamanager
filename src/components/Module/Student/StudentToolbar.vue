ab4f701128d0326a78d6d78fee7d5276ee3e7ad2
qixiaojian310 <1821059387@qq.com>
2022-02-20 13:39:55 +0800

    yaoyao

--- src/components/Module/Student/StudentToolbar.vue
+++ src/components/Module/Student/StudentToolbar.vue
@@ -4,25 +4,38 @@
     <!-- 下首先应该有总览，上会出现各种排序方式加一个搜索框，右边是搜索结果
     下有加入，上面有搜素框，右边是搜索结果 -->
     <div class="toolbar-list">
-      <sign-input-item :name="'Search'" v-model:inputValue="SearchCOntent" :fontSize="'1.2rem'" :backgroundColor="'#e6e1e1'" :inputHeight="2"></sign-input-item>
+      <sign-input-item
+        :name="'Search'"
+        v-model:inputValue="SearchCOntent"
+        :fontSize="'1.2rem'"
+        :backgroundColor="'#e6e1e1'"
+        :inputHeight="2"
+      ></sign-input-item>
     </div>
     <div class="toolbar-list">
-
+      <a class="btn-all-module">
+        <i class="fa fa-bars"></i>
+        ALL Modules
+      </a>
+      <a class="btn-add-module" >
+        <i class="fa fa-plus-square-o" ></i>
+        ADD Module
+      </a>
     </div>
   </div>
 </template>
 
 <script>
-import SignInputItem from '../../sign/SignInputItem.vue'
+import SignInputItem from "../../sign/SignInputItem.vue";
 export default {
   components: { SignInputItem },
-  name: 'StudentToolbar',
-  data(){
-    return{
-      SearchCOntent: '',
-    }
-  }
-}
+  name: "StudentToolbar",
+  data() {
+    return {
+      SearchCOntent: "",
+    };
+  },
+};
 </script>
 
 <style>
@@ -33,9 +46,38 @@ export default {
   margin-top: 5%; */
   background-color: #e6e1e1;
 }
-.toolbar-list{
-  height:45%;
+.toolbar-list {
+  height: 45%;
   padding: 5%;
   width: 90%;
+}
+
+.btn-all-module {
+  transition: background-color 0.35s ease-in-out, color 0.35s ease-in-out;
+  text-decoration: none;
+  color: #df7599;
+  background-color: #fac1d3;
+  border: 2px solid #df7599;
+  width: 80%;
+  display: block;
+  text-align: center;
+  padding: 2%;
+  border-radius: 10px;
+  font-size: 1.2rem;
+}
+
+.btn-add-module {
+  transition: background-color 0.35s ease-in-out, color 0.35s ease-in-out;
+  text-decoration: none;
+  color: #066e14;
+  background-color: #65b54d56;
+  border: 2px solid #65b54d;
+  width: 80%;
+  display: block;
+  text-align: center;
+  padding: 2%;
+  margin-top: 10px;
+  border-radius: 10px;
+  font-size: 1.2rem;
 }
 </style>
---
+++ src/components/Module/Student/ToolBarButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <input
+      class="btn-all-module"
+      type="button"
+      value="ALL Modules"/>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.btn-all-module {
+  transition: background-color 0.35s ease-in-out, color 0.35s ease-in-out;
+  text-decoration: none;
+  color: #1b1b1b;
+  background-color: #65b54d56;
+  border: 2px solid #65b54d;
+  width: 80%;
+  display: block;
+  text-align: center;
+  padding: 2%;
+  border-radius: 10px;
+  font-size: 1.2rem;
+}
+</style>
